20561a81a25724c1b279a28442fe3026d38614e0
doitaka <doitkhr@gmail.com>
2016-10-19 09:36:45 +0900

    exercise 1.12

--- chap1/1_12.scm
+++ chap1/1_12.scm
@@ -1,17 +1,29 @@
+;; input : row number, column number
+;; output : number
 
-(define (print-pascal-line n)
-  (print n))
+;; row number and quantity of row's number is same num.
+;; row's most left num and most right num is always 1.
+;; 
+;; line[x][y]
+;; if x == 1
+;; 	line[x][y] = 1
+;; if x > 1
+;; 	line[x][y] = line[x-1][y-1] + line[x-1][y]
+
+;; always x >= y
+
+;; if input is invalid, returns 0.
 
 
-(define (print-pascal-iter count n)
-  (cond ((<= count n) (print-pascal-line count)
-					  (print-pascal-iter (+ count 1) n))))
-
-(define (print-pascal n)
-  (cond ((>= n 1) (print-pascal-iter 1 n))))
+(define (pascal-iter n m)
+  (cond ((< n 1) 0)
+	((< m 1) 0)
+	((< n m) 0)
+	((= n 1) 1)
+	((> n 1) (+ (pascal-iter (- n 1) (- m 1)) (pascal-iter (- n 1) m)))))
+	
 
 (define (main args)
-  (apply print-pascal (map string->number (cdr args)))
+  (print (apply pascal-iter (map string->number (cdr args))))
   0)
-
 
